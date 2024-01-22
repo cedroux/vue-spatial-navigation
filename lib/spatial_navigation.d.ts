85f301cb595a6b95c9744cd22a10ed3b6c3760c3
Cédric Roux <cedroux42@gmail.com>
2024-01-22 17:39:07 +0100

    Improved types definition (thanks @Reeska)

--- lib/spatial_navigation.d.ts
+++ lib/spatial_navigation.d.ts
@@ -1,27 +1,29 @@
 /// <reference lib="dom" />
 
-declare namespace SpatialNavigation {
-  function init(): void;
-  function uninit(): void;
-  function clear(): void;
-  function reset(sectionId?: string): void;
-  function set(config?: object): void;
-  function set(sectionId?: string, config?: object): void;
-  function add(config?: object): string;
-  function add(sectionId?: string, config?: object): string;
-  function remove(sectionId?: string): boolean;
-  function disable(sectionId?: string): boolean;
-  function enable(sectionId?: string): boolean;
-  function pause(): void;
-  function resume(): void;
-  function focus(silent?: boolean): boolean;
-  function focus(sectionId?: string, silent?: boolean): boolean;
-  function focus(extSelector?: unknown, silent?: boolean): boolean;
-  function move(direction: 'left' | 'up' | 'right' | 'down', selector?: HTMLElement): boolean;
-  function makeFocusable(sectionId?: string): void;
-  function setDefaultSection(sectionId?: string): void;
-  function getCurrentFocusedElement(): HTMLElement;
+interface SpatialNavigation {
+  init(): void;
+  uninit(): void;
+  clear(): void;
+  reset(sectionId?: string): void;
+  set(config?: object): void;
+  set(sectionId?: string, config?: object): void;
+  add(config?: object): string;
+  add(sectionId?: string, config?: object): string;
+  remove(sectionId?: string): boolean;
+  disable(sectionId?: string): boolean;
+  enable(sectionId?: string): boolean;
+  pause(): void;
+  resume(): void;
+  focus(silent?: boolean): boolean;
+  focus(sectionId?: string, silent?: boolean): boolean;
+  focus(extSelector?: unknown, silent?: boolean): boolean;
+  move(direction: 'left' | 'up' | 'right' | 'down', selector?: HTMLElement): boolean;
+  makeFocusable(sectionId?: string): void;
+  setDefaultSection(sectionId?: string): void;
+  getCurrentFocusedElement(): HTMLElement;
 }
+
+declare const SpatialNavigation: SpatialNavigation;
 
 export const EVENT_PREFIX: string;
 export default SpatialNavigation;
